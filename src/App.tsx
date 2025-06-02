338c3b8b0b331e8fd8f08cf52bc2e4a635c41565
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-06-01 18:42:52 -0700

    Set up card grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { Button } from "@/components/ui/button"
-import { useState } from "react"
+import CardGrid from "@/components/ui/card_grid";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount((prevCount) => prevCount + 1);
-  }
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <Button 
-      className="flex flex-col items-center justify-center"
-      onClick={handleClick}>
-      Count is {count}
-    </Button>
+      <CardGrid />
     </div>
     
   )
---
+++ src/components/ui/card_grid.tsx
@@ -0,0 +1,38 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardTitle,
+} from "@/components/ui/card";
+
+function CardGrid() {
+    const handleCardClick = () => {
+        alert('Card clicked');
+    };
+
+    const numCards = 8; // Number of cards to display
+    const cards = Array.from({ length: numCards }, (_, i) => `Card ${i + 1}`);
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+            {cards.map((description, index) => (
+                
+                // Want card to be clickable instead of using a button
+                <Card key={index} className="cursor-pointer" onClick={handleCardClick}>
+                    <CardAction>
+                        <CardContent>
+                            <CardTitle>{description}</CardTitle>
+                            <CardDescription>
+                                This is a description for {description}.
+                            </CardDescription>
+                        </CardContent>
+                    </CardAction>
+                </Card>
+            ))}
+        </div>
+    );
+
+}
+
+export default CardGrid;
